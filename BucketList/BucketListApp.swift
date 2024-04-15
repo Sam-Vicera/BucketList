8a1d552ae74afaf195a14312d6cf594df78432de
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-04-15 10:51:38 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Samuel Hernandez Vicera on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
